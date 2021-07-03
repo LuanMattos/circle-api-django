128d2b4645139f96cda1c0b04f077ef05d023e0e
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-07-03 17:18:02 -0300

    Finalizacao de statistic words photo

--- photo_statistic/serializer.py
+++ photo_statistic/serializer.py
@@ -14,12 +14,19 @@ class PhotoStatisticSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PhotoStatistic
-        fields = ('statistic','photo_statistic_id')        
+        fields = '__all__'       
 
     def get_statistic(self, statistic):
         item = statistic.photo.__dict__
+        item_statistic = statistic.__dict__
+        import pprint
+        pp = pprint.PrettyPrinter(indent=4)
+        
         stop_words = set(stopwords.words('portuguese'))
+        stop_words_english = set(stopwords.words('english'))
         words = item['photo_description']
+        time = item_statistic['photo_statistic_time']
+
         total_words = words.split()
         total_word_length = len(total_words)
         total_sentences = tokenize.sent_tokenize(words)
@@ -28,15 +35,15 @@ class PhotoStatisticSerializer(serializers.ModelSerializer):
         tf_score = {}
         for each_word in total_words:
             each_word = each_word.replace('.','')
-            if each_word not in stop_words:
+            if each_word not in stop_words and each_word not in stop_words_english:
                 if len(each_word) > 2:
                     if each_word in tf_score:
                         tf_score[each_word] += 1
                     else:
                         tf_score[each_word] = 1
             
-        tf_score.update((x, y/int(total_word_length)) for x, y in tf_score.items())
-        photos = Photo.objects.all()
+        tf_score.update((x, y/int(total_word_length) + time) for x, y in tf_score.items())
+        # photos = Photo.objects.all()
         
         return tf_score
     
